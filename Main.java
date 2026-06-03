acc4606e00c96e1940e5945bd8e8f21183c138a9
Job Daniel Correa <jobcorre0912@gmail.com>
2026-06-03 14:22:37 -0500

    Prubas de interfacez Cuentas Bancarias

--- Main.java
+++ Main.java
@@ -4,5 +4,21 @@ public class Main{
         Reparacion rep = new Tecnico();
         aseo.limpiar();
         rep.reparar();
+        System.out.println();
+
+        System.out.println("=== EJERCICIO 2: Operaciones Bancarias ===");
+        CuentaAhorro ahorros = new CuentaAhorro();
+        ahorros.transferir();
+        ahorros.retirar();
+        System.out.println("----------------");
+        CuentaEmpresarial empresarial = new CuentaEmpresarial();
+        empresarial.pagar();
+        System.out.println("----------------");
+        CuentaCorriente corriente = new CuentaCorriente();
+        corriente.transferir();
+        corriente.retirar();
+        corriente.pagar();
+        System.out.println();
+ 
     }
 }
