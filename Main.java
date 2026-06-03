c1ac44b4505a830d8f1565a51b4673020d94ee37
Job Daniel Correa <jobcorre0912@gmail.com>
2026-06-03 13:03:18 -0500

    Inizacion de interfaz

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Limpieza aseo = new PersonalAseo();
+        Reparacion rep = new Tecnico();
+        aseo.limpiar();
+        rep.reparar();
     }
 }
