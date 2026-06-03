ed04bf25408a87ae779d4491730b2e5e96461c49
Job Daniel Correa <jobcorre0912@gmail.com>
2026-06-03 13:01:30 -0500

    Ejercicio #1- separacion por interfaz

---
+++ PersonalAseo.java
@@ -0,0 +1,6 @@
+public class PersonalAseo implements Limpieza{
+    @Override
+    public void limpiar(){
+        System.out.println("Personal de Aseo limpiando Pasillo 9");
+    }
+}
---
+++ Tecnico.java
@@ -0,0 +1,6 @@
+public class Tecnico implements Reparacion{
+    @Override
+    public void reparar(){
+        System.out.println("El tecnico esta reparando.......");
+    } 
+}
